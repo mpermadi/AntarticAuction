d4b83526839f2aee0d857e2f4c1abf161a7f7c71
Marcelino Permadi <mpermadi@indo-soft.com>
2014-11-05 05:05:20 -0500

    simple test to check main menu link

---
+++ tests/acceptance/SigninCept.php
@@ -0,0 +1,22 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('perform actions login');
+$I->amOnPage('/');
+$I->see('Antarctic Auction');
+$I->seeLink('Home');
+$I->click('Home');
+$I->seeInCurrentUrl('/');
+$I->seeLink('Current Inventory');
+$I->click('Current Inventory');
+$I->see('All Posted Items');
+$I->seeInCurrentUrl('/auction-list');
+$I->seeLink('How it Works');
+$I->click('How it Works');
+$I->seeInCurrentUrl('/how-it-works');
+$I->seeLink('About');
+$I->click('About');
+$I->see('About the US Antarctic Auction');
+$I->seeInCurrentUrl('/about');
+$I->seeLink('Contact');
+$I->click('Contact');
+$I->seeInCurrentUrl('/contact');
